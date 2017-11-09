ffe71f2cfabdb35b1898883f071fa4803fecbe80
Ruby <c19890611@gmail.com>
2017-11-08 23:14:46 -0600

    export to PDF

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
   //mic basic setting
-  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-  const recognition = new SpeechRecognition();
-  recognition.interimResults = true;
-  recognition.lang = 'en-US';
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.lang = 'en-US';
 
-  let p = document.createElement('p');
-  const words = document.querySelector('.words');
-  words.appendChild(p);
+// render text to show up
+    let p = document.createElement('p');
+    const words = document.querySelector('.words');
+    words.appendChild(p);
+
+    //map through the result to get transcript only
+
+    recognition.addEventListener('result', e => {
+      const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+
+// make special symbol for say specific word
+        const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+        p.textContent = poopScript;
+
+// checking is ending the converasion, and don't rewrite the previous one.
+
+        if (e.results[0].isFinal) {
+          p = document.createElement('p');
+          words.appendChild(p);
+        }
+    });
+
+    recognition.addEventListener('end', recognition.start);
+
+    recognition.start();
 
 
-  // map throught the data, grab the transcript only
-  recognition.addEventListener('result', e => {
-    const transcript = Array.from(e.results)
-      .map(result => result[0])
-      .map(result => result.transcript)
-      .join('');
-    console.log(transcript)
 
-    p.textContent = transcript
+//export to PDF
 
-    // don't let the speak rewrite the container
-    if (e.results[0].isFinal) {
-      p = document.createElement('p');
-      words.appendChild(p);
-    }
-  });
+  let doc = new jsPDF();
+  let specialElementHandlers = {
+      '#editor': function (element, renderer) {
+          return true;
+      }
+  };
 
-// pause and keep taking will continue the sentence
-  recognition.addEventListener('end', recognition.start);
-  recognition.start();
+  $('.button').click(function () {
+    doc.fromHTML($('.words').html(), 15, 15, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+    doc.save('sample.pdf');
+});
